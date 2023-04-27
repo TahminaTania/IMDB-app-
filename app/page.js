00692c9150a862b02398413c140f9817c92826df
user-tania <usertania@gmail.com>
2023-04-27 19:50:06 +0600

    fetch data from tmdb api

--- app/page.js
+++ app/page.js
@@ -1,9 +1,33 @@
-import Image from 'next/image'
+const API_KEY=process.env.API_KEY
 
-export default function Home() {
+export default async function Home({searchParams}) {
+  const genre=searchParams.genre || "fetchTrending"
+  const res=await fetch(`https:/api.themoviedb.org/3/${
+    genre==="fetchTopRated"? "movie/top_rated":"trending/all/week"
+  }?api_key=${API_KEY}&language=en-US&page=1`,
+  { next:{ revalidate:10000
+  }});
+
+  // if(!res.ok){
+  //   throw new Error("Failed to Fetch Data")
+  // }
+  const data=await res.json();
+   const result=data.results;
+  console.log(result, "those are resultss...")
   return (
     <main className="">
-      Hiii
+      result
+      <div>
+      {result.map((data)=>{
+        return(
+          <div key={data.id}>
+          <p className="">{data.title}</p> 
+            </div>
+        )
+      })
+
+      }
+      </div>
     </main>
   )
 }
